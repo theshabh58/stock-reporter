1bde525bd13074395dc65ce170acdfd1fa82d28f
vimalp19 <vphb9@umsl.edu>
2020-06-15 21:59:31 -0500

    Implemented the addition of 'AddStock' function. Corrected struct tags that were incorrectly defined previously.

--- apis/addstock.go
+++ apis/addstock.go
@@ -7,14 +7,20 @@ import (
 	"os"
 )
 
-//AddUser adds a new user for stock reports
-func AddUser(w http.ResponseWriter, r *http.Request) {
+//AddStockRequest request body for AddStock endpoint
+type AddStockRequest struct {
+	UserEmail   string `json:"userEmail"`
+	StockTicker string `json:"stockTicker"`
+}
+
+//AddStock adds a new stock to the given user
+func AddStock(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	//Get request body
 	decoder := json.NewDecoder(r.Body)
-	var user User
-	err := decoder.Decode(&user)
+	var input AddStockRequest
+	err := decoder.Decode(&input)
 	if err != nil {
 		log.Println("Invalid request body provided")
 		w.WriteHeader(http.StatusBadRequest)
@@ -22,7 +28,7 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Verify all properties in the request body are provided
-	if user.FirstName == "" || user.LastName == "" || user.Email == "" {
+	if input.UserEmail == "" || input.StockTicker == "" {
 		log.Println("Missing data in the request body")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -43,25 +49,23 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//Build sock report to insert
-	stockReport := StockReport{
-		User:   user,
-		Stocks: []Stock{},
-	}
-
-	//Insert into the database
-	id, err := db.InsertStockReport(ctx, stockReport, collectionName)
+	//Find the matching stock report using the given email
+	id, err := db.GetStockReportByEmail(ctx, input.UserEmail, collectionName)
 
 	if err != nil {
-		log.Println("Error inserting post into the database:", err)
+		log.Println("Error finding stock report with the provided email:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	//Proceed with returning response if no errors encountered
-	stockReport.ID = id
-	log.Println("Successfully inserted new stock report:", id)
+	//Update that stock report by adding the new stock
+	err = db.AddNewStock(ctx, input.StockTicker, id, collectionName)
 
-	json.NewEncoder(w).Encode(stockReport)
+	if err != nil {
+		log.Println("Error adding the provided stock:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusCreated)
 }
