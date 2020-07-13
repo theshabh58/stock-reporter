8918181bd27f9e9ab1fdb8fc5bc0f12f162b9bef
Rishabh <theshabh58@gmail.com>
2020-07-12 20:12:24 -0500

    added dynamic templates with handlebars on the front-end to populate stock values to send out an email

--- apis/emailstockreport.go
+++ apis/emailstockreport.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/smtp"
@@ -16,12 +17,30 @@ import (
 var tpl *template.Template
 var emailAuth smtp.Auth
 
-//EmailParameters ...
-type EmailParameters struct {
-	To      []string
-	From    string
-	Subject string
-	Message []byte
+//DymanicRequest ...
+type DymanicRequest struct {
+	Personalizations []DataValues      `json:"personalizations"`
+	From             map[string]string `json:"from"`
+	Content          []ContentType     `json:"content"`
+	TemplateID       string            `json:"template_id"`
+}
+
+type DataValues struct {
+	UserEmails []UserData        `json:"to"`
+	Message    StockReportValues `json:"dynamic_template_data"`
+}
+
+type UserData struct {
+	EmailAddress string `json:"email"`
+	Name         string `json:"name"`
+}
+type ContentType struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type StockReportValues struct {
+	StockInfo []StockData `json:"stock_data"`
 }
 
 func init() {
@@ -40,7 +59,7 @@ func GetStockReportFromTopic(ctx context.Context, msg []byte) error {
 		return err
 	}
 
-	err = SendEmail(report)
+	err = SendDynamicTemplateEmail(report)
 	if err != nil {
 		return err
 	}
@@ -52,8 +71,6 @@ func GetStockReportFromTopic(ctx context.Context, msg []byte) error {
 func SendEmail(report StockReport) error {
 	//buffer to store the templated stock values
 	buffer := new(bytes.Buffer)
-	// headers := "MIME-version: 1.0;\nContext-Type: text/html;"
-	// buffer.Write([]byte(fmt.Sprintf("Subject: StockReport %s\n", headers)))
 
 	//populate the report to buffer
 	err := tpl.Execute(buffer, report)
@@ -61,13 +78,14 @@ func SendEmail(report StockReport) error {
 		return err
 	}
 
-	//setup fields to send email
+	//setup fields to send email through sendgrid
 	to := mail.NewEmail(report.User.FirstName, report.User.Email)
 	subject := "Stock Report"
-	from := mail.NewEmail("Stock-reporter @", os.Getenv("FROM_EMAIL"))
+	from := mail.NewEmail("Stock-Report", os.Getenv("FROM_EMAIL"))
 	htmlContent := buffer.String()
 	email := mail.NewSingleEmail(from, subject, to, "None NEeded", htmlContent)
 
+	//setup sendgrid client
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	resp, err := client.Send(email)
 	if err != nil {
@@ -75,4 +93,30 @@ func SendEmail(report StockReport) error {
 	}
 	log.Printf("StatusCode: %v\n, ResponseBody: %v\n, ResponseHeader: %v\n", resp.StatusCode, resp.Body, resp.Headers)
 	return nil
+}
+
+//SendDynamicTemplateEmail ...
+func SendDynamicTemplateEmail(report StockReport) error {
+	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
+
+	params, _ := json.Marshal(getPopulatedRequest(report))
+	fmt.Printf("Request Body %s", params)
+	request.Method = "POST"
+	request.Body = params
+
+	resp, err := sendgrid.API(request)
+	if err != nil {
+		return err
+	}
+	log.Printf("StatusCode: %v\n, ResponseBody: %v\n, ResponseHeader: %v\n", resp.StatusCode, resp.Body, resp.Headers)
+	return nil
+}
+
+func getPopulatedRequest(report StockReport) DymanicRequest {
+	var req DymanicRequest
+	req.Content = []ContentType{{Type: "text/html", Value: "1.1"}}
+	req.Personalizations = []DataValues{{UserEmails: []UserData{{Name: report.User.FirstName, EmailAddress: report.User.Email}}, Message: StockReportValues{report.StockReport}}}
+	req.From = map[string]string{"email": os.Getenv("FROM_EMAIL")}
+	req.TemplateID = os.Getenv("TEMPLATE_ID")
+	return req
 }
